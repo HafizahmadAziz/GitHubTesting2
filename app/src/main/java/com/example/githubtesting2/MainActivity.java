817f1b1acd7ec3f4259fcc3bfd0778b95bf18dc0
ahmad <hahmadaziz@gmail.com>
2019-04-22 12:56:56 +0500

    Testing 2

--- app/src/main/java/com/example/githubtesting2/MainActivity.java
+++ app/src/main/java/com/example/githubtesting2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.githubtesting2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        Toast.makeText ( this, "Testing 2", Toast.LENGTH_SHORT ).show ();
+
     }
 }
